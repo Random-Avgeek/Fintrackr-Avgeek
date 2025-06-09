17824af4b728c24aafdac9beae66bc3a889a0d99
Random-Avgeek <arushbhatnagar05@gmail.com>
2025-06-09 17:42:15 +0530

    Updated frontend code to connect with working backend

--- src/context/TransactionContext.jsx
+++ src/context/TransactionContext.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const TransactionContext = createContext();
 
-const API_URL = 'http://127.0.0.1:5000/api';
+const API_URL = 'https://avgeek-fintrackr-services.onrender.com/api';
 
 const initialState = {
   transactions: [],
